9e8c7dd347b09886920c9ebf13f1fc62f6db494e
Wazeer <58399263+wazeerc@users.noreply.github.com>
2025-01-19 14:24:39 +0400

    chore: update title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Wazeer's Blog";
-export const SITE_DESCRIPTION = "Welcome to my blog!";
+export const SITE_TITLE = "Wazeer Chadun • Personal Website";
+export const SITE_DESCRIPTION = "Discover Wazeer's projects, blog posts, and get to know more about him and his journey in tech.";
