461d77c4cb498c2a94f2bc7be7472896ac20be8d
Wazeer <58399263+wazeerc@users.noreply.github.com>
2025-01-22 11:54:47 +0400

    fix: update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://wazeerc.github.io",
   integrations: [mdx(), sitemap()],
 });
