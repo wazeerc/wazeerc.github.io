1676c2593c6580f42c9e418bdac5b70400e97305
Wazeer <58399263+wazeerc@users.noreply.github.com>
2025-02-02 10:22:34 +0400

    chore: run prettier

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = "Wazeer Chadun • Personal Website";
-export const SITE_DESCRIPTION = "Discover Wazeer's projects, blog posts, and get to know more about him and his journey in tech.";
+export const SITE_DESCRIPTION =
+  "Discover Wazeer's projects, blog posts, and get to know more about him and his journey in tech.";
